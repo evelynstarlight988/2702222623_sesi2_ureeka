82460eb1f6c97a17c8405dcf0a09b3dde80b0758
evelynstarlight988 <evelynstarlight988@gmail.com>
2025-05-20 20:38:41 +0700

    Tugas 2

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+
+
+const nextConfig = {
+  reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
